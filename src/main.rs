c85ba6584e82da998ff1c18bbd004250bda50fb9
oiwn <alex@imscraping.ninja>
2022-01-05 02:50:52 +0700

    parse scale name into struct

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,48 @@ enum Commands {
     Chords,
 }
 
-const scale_sharps: Vec<&str> = vec!["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-const scale_flats: Vec<&str> = vec!["C", "Db", "D", "Eb", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+#[derive(Debug)]
+enum Accidentals { Sharp, Flat, Natural }
+#[derive(Debug)]
+enum ScaleTypes { Maj, Min }
+
+const scale_sharps: [&str; 12] = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const scale_flats: [&str; 12] = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+
+const major_scale_rule: [u8; 7] = [2, 2, 1, 2, 2, 2, 1];  // in semitones
+const nat_minor_scale_rule: [u8; 7] = [2, 1, 2, 2, 1, 2, 2];  // in semitones
+
+#[derive(Debug)]
+struct Scale {
+    key: String,
+    accidental: Accidentals,
+    scale_type: ScaleTypes,
+}
+
+fn parse_scale(scale_name: &str) -> Result<Scale, &str> {
+    if scale_name.len() < 4 {
+        return Err("Error parsing scale name")
+    }
+    let accidental = match scale_name.chars().nth(1) {
+        Some('#') => Accidentals::Sharp,
+        Some('b') => Accidentals::Flat,
+        _ => Accidentals::Natural
+    };
+    let key = match accidental {
+        Accidentals::Sharp | Accidentals::Flat => scale_name[..2].to_string(),
+        Accidentals::Natural => scale_name[..1].to_string() 
+    };
+    let scale_type_substr = match accidental {
+        Accidentals::Sharp | Accidentals::Flat => scale_name[2..].to_string(),
+        Accidentals::Natural => scale_name[1..].to_string()
+    };
+    let scale_type = match scale_type_substr.as_str() {
+        "maj" => ScaleTypes::Maj,
+        "min" => ScaleTypes::Min,
+        _ => return Err("Error parsing scale name, wrong scale type")
+    };
+    Ok(Scale { key, accidental, scale_type })
+}
 
 fn notes_in_scale(scale_name: &str) -> String {
     "A-B-C-D-E-F-G-A".to_string()
@@ -26,17 +66,15 @@ fn notes_in_scale(scale_name: &str) -> String {
 fn main() {
     println!("Scales and Chords little helper");
     let cli = Cli::parse();
-    match &cli.command {
+    match cli.command {
         Commands::Scale { name } => {
-            let scale_name = match name {
-                None => "Amin".to_string(),
-                Some(x) => x.to_string()
-            };
-            println!("Notes in scale {:?} : {:?}", scale_name, notes_in_scale(&scale_name));
+            let scale_name: String = name.unwrap_or("Cmaj".to_string());
+            println!("Scale name: {:?}", scale_name);
+            println!("Scale: {:#?}", parse_scale(&scale_name).unwrap());
+            // println!("Notes in scale {:?} : {:?}", scale_name, notes_in_scale(&scale_name));
         }
         Commands::Chords => {
             println!("Hello");
         }
     } 
-    println!("Hello, world!");
 }
